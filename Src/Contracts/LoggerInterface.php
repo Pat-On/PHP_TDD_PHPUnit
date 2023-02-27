d1c2e7373fd6499d4b16e2ffc5f6fb4693b0fbaf
Patryk <patryk.r.nowak@gmail.com>
2023-02-27 05:42:38 +0000

    logger - psr-3 standard

---
+++ Src/Contracts/LoggerInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Contracts;
+
+
+interface LoggerInterface
+{
+    // (debug, info, notice, warning, error, critical, alert, emergency).
+    // php-fig.org/psr/psr-3/
+
+    public function emergency(string $message, array $context = [])
+    {
+    }
+
+    public function alert(string $message, array $context = [])
+    {
+    }
+
+    public function critical(string $message, array $context = [])
+    {
+    }
+
+    public function error(string $message, array $context = [])
+    {
+    }
+
+    public function warning(string $message, array $context = [])
+    {
+    }
+
+    public function info(string $message, array $context = [])
+    {
+    }
+
+    public function debug(string $message, array $context = [])
+    {
+    }
+
+    public function log(string $level, string $message, array $context = [])
+    {
+    }
+}
